983d8a3a4cb8aac89af9993bae1d76bf244c5113
SkyForce06 <01192876@pw.edu.pl>
2023-12-14 15:24:23 +0100

    Implementacja metody eliminacji Gaussa

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,25 @@
 #include "gauss.h"
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+int eliminate(Matrix *mat, Matrix *b) {
+    int n = mat->rows;
 
-		return 0;
+    for (int k = 0; k < n - 1; k++) {
+        // Eliminacja współczynników poniżej diagonali
+        for (int i = k + 1; i < n; i++) {
+            double factor = mat->data[i][k] / mat->data[k][k];
+            b->data[i][0] -= factor * b->data[k][0];
+            for (int j = k; j < n; j++) {
+                mat->data[i][j] -= factor * mat->data[k][j];
+            }
+        }
+    }
+
+    // Sprawdź, czy macierz jest osobliwa
+    for (int i = 0; i < n; i++) {
+        if (mat->data[i][i] == 0.0) {
+            return 1; // Macierz osobliwa
+        }
+    }
+
+    return 0; // Eliminacja zakończona sukcesem
 }
-
